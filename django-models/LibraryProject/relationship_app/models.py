177ef5a82686168b278a59904876515ba85cd15e
lohochris <159076124+lohochris@users.noreply.github.com>
2025-02-21 19:59:59 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
-
-    class Meta:
-        permissions = [
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
-        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
